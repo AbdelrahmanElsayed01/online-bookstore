f9429499db9d29c2b4355ded6ff693dd7066aa69
AbdelrahmanElsayed01 <165659493+AbdelrahmanElsayed01@users.noreply.github.com>
2025-12-05 19:47:18 +0100

    payment service cicd

--- services/api-gateway/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ services/api-gateway/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bad65c6209db373b6b60bf5366759b2fe44b21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c06d772f0b386d5a3e319144ad10bba0205e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
